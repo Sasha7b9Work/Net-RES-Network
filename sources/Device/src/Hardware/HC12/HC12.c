d2c734cb15e13bf9a74b2dd6dc0fd5669f3618ed
Sasha7b9 <Sasha7b9@tut.by>
2022-01-27 13:25:42 +0300

    *

--- sources/Device/src/Hardware/HC12/HC12.c
+++ sources/Device/src/Hardware/HC12/HC12.c
@@ -1,8 +1,20 @@
 #include "Hardware/HC12/HC12.h"
+#include "stm32f1xx_hal.h"
 
 
 void HC12_Init()
 {
+    GPIO_InitTypeDef  GPIO_InitStruct;
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_USART1_CLK_ENABLE();
+
+    GPIO_InitStruct.Pin       = GPIO_PIN_9 | GPIO_PIN_10;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_PULLUP;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 
 
