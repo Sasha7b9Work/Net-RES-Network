e778c12fd1b6e4420e2a42b267eb85de1922ab33
Sasha7b9 <Sasha7b9@tut.by>
2022-01-27 15:39:38 +0300

    Не работает

--- sources/Device/src/Hardware/HC12/HC12.c
+++ sources/Device/src/Hardware/HC12/HC12.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include "Hardware/HC12/HC12.h"
 #include "stm32f1xx_hal.h"
 #include <string.h>
@@ -13,11 +14,16 @@ void HC12_Init()
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_USART1_CLK_ENABLE();
 
-    GPIO_InitStruct.Pin       = GPIO_PIN_9 | GPIO_PIN_10;
+    GPIO_InitStruct.Pin       = GPIO_PIN_9;
     GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull      = GPIO_PULLUP;
     GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
 
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    
+    GPIO_InitStruct.Pin  = GPIO_PIN_10;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     UartHandle.Instance        = USART1;
@@ -25,17 +31,20 @@ void HC12_Init()
     UartHandle.Init.BaudRate   = 9600;
     UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
     UartHandle.Init.StopBits   = UART_STOPBITS_1;
-    UartHandle.Init.Parity     = UART_PARITY_ODD;
+    UartHandle.Init.Parity     = UART_PARITY_NONE;
     UartHandle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
     UartHandle.Init.Mode       = UART_MODE_TX_RX;
 
     if (HAL_UART_Init(&UartHandle) != HAL_OK)
     {
+        Error_Handler();
     }
 }
 
 
 void HC12_Send(char *buffer)
 {
-    HAL_UART_Transmit(&UartHandle, (uint8_t *)buffer, strlen(buffer), 0xFFFF);
+    HAL_StatusTypeDef res = HAL_UART_Transmit(&UartHandle, (uint8_t *)buffer, strlen(buffer), 0xFFFF);
+    
+    res = res;
 }
