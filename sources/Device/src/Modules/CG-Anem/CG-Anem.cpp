2a382eb7e24245ece6ee612997ffe78e98377998
Sasha7b9 <Sasha7b9@tut.by>
2022-02-14 12:05:32 +0300

    *

--- sources/Device/src/Modules/CG-Anem/CG-Anem.cpp
+++ sources/Device/src/Modules/CG-Anem/CG-Anem.cpp
@@ -1,4 +1,30 @@
 // 2022/02/14 11:59:20 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "main.h"
 #include "Modules/CG-Anem/CG-Anem.h"
+#include <stm32f1xx_hal.h>
 
+
+namespace CG_Anem
+{
+    static unsigned int timeNext = 1;
+}
+
+
+void CG_Anem::Init()
+{
+
+}
+
+
+const char *CG_Anem::GetMeasure(unsigned int dT)
+{
+    static char buffer[128];
+
+    while (HAL_GetTick() < timeNext)
+    {
+    }
+
+    timeNext += dT;
+
+    return buffer;
+}
