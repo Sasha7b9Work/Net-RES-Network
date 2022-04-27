7b8abf3dd69caf6924881d826865b222e7567e45
Sasha7b9 <Sasha7b9@tut.by>
2022-04-27 14:36:34 +0300

    *

---
+++ sources/common/_VS_LIBS/STM32F1/stm32f1xx_hal_gpio.cpp
@@ -0,0 +1,8 @@
+// 2022/04/27 14:36:00 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include <stm32f1xx_hal.h>
+
+
+void HAL_GPIO_WritePin(GPIO_TypeDef * /*GPIOx*/, uint16_t /*GPIO_Pin*/, GPIO_PinState /*PinState*/)
+{
+
+}
