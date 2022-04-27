3aecae8364599493076bb67cc18391252f227c73
Sasha7b9 <Sasha7b9@tut.by>
2022-04-27 11:52:11 +0300

    *

--- sources/VS/Device/main_win.cpp
+++ sources/VS/Device/main_win.cpp
@@ -1,14 +1,15 @@
 #define WIN32_LEAN_AND_MEAN
 #include "Application.h"
+#include "Device.h"
 
 
 void Application::Init()
 {
-
+    Device::Init();
 }
 
 
 void Application::Update()
 {
-
+    Device::Update();
 }
