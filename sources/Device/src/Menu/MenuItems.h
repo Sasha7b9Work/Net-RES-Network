f7185c3fb9783607a33ef10e48540895b99f4951
Sasha7b9 <Sasha7b9@tut.by>
2022-05-05 13:03:13 +0300

    *

--- sources/Device/src/Menu/MenuItems.h
+++ sources/Device/src/Menu/MenuItems.h
@@ -1,3 +1,32 @@
 // 2022/05/05 12:27:33 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 
+
+struct TypeItem
+{
+    enum E
+    {
+        Page,
+        Choice,
+        Count
+    };
+};
+
+
+
+struct Item
+{
+    TypeItem::E type;
+};
+
+
+struct Page : public Item
+{
+
+};
+
+
+struct Choice : public Item
+{
+
+};
