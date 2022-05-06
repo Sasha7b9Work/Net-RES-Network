a8530f8a360ee165cafff37979cd840e40160606
Sasha7b9 <Sasha7b9@tut.by>
2022-05-06 14:51:21 +0300

    *

--- sources/Device/src/Menu/MenuItemsDefs.h
+++ sources/Device/src/Menu/MenuItemsDefs.h
@@ -3,24 +3,10 @@
 
 
 #define DEF_BUTTN(name, title, keeper, function)    \
-    static const DButton name =                     \
-    {                                               \
-        TypeItem::Button,                           \
-        title,                                      \
-        (const Page *)&keeper,                      \
-        function                                    \
-    };
+    static const DButton name = { TypeItem::Button, title, (const Page *)&keeper, function };
 
 #define DEF_CHOICE_2(name, title, keeper, cell, name1, name2)   \
-    static const DChoice name =                                 \
-    {                                                           \
-        TypeItem::Choice,                                       \
-        title,                                                  \
-        (const Page *)&keeper,                                  \
-        &cell, 2,                                               \
-        name1, name2                                            \
-    };
-
+    static const DChoice name = { TypeItem::Choice, title, (const Page *)&keeper, &cell, 2, name1, name2 };
 
 #define DEF_ITEMS_1(name, item)                                                             \
     static const Item * const name[] = { (Item *)&item, nullptr };
@@ -35,11 +21,9 @@
     static const Item * const name[] = { (Item *)&item1, (Item *)&item2, (Item *)&item3,    \
         (Item *)&item4, (Item *)&item5, (Item *)&item6, nullptr };
 
-
 #define DEF_PAGE(name, title, keeper, items)                                                \
     static uint8 ci##name = 0;                                                              \
     const DPage name = { TypeItem::Page, title, (const Page *)&keeper, items, &ci##name };
-
 
 #define DEF_PAGE_1(name, title, keeper, item1)  \
     DEF_ITEMS_1(items##name, item1)             \
