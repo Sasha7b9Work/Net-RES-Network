f9a48b46c3e32affa6a03c69e220db3604af7a2d
Sasha7b9 <Sasha7b9@tut.by>
2022-05-11 15:31:24 +0300

    *

--- sources/Device/src/Hardware/I2C/i2c.h
+++ sources/Device/src/Hardware/I2C/i2c.h
@@ -1,25 +1,24 @@
-#ifndef __i2c_H
-#define __i2c_H
-#ifdef __cplusplus
- extern "C" {
-#endif
-
+#pragma once
 #include "stm32f1xx_hal.h"
 #include "defines.h"
 
-extern I2C_HandleTypeDef hi2c1;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern void _Error_Handler(char *, int);
+    extern I2C_HandleTypeDef hi2c1;
 
-void MX_I2C1_Init(void);
+    extern void _Error_Handler(char *, int);
 
-void user_delay_ms(uint32_t period);
-int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
-int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
+    void MX_I2C1_Init(void);
 
-int8_t user_i2c_write8(uint8_t dev_id, uint8_t data);
-int8_t user_i2c_read16(uint8_t dev_id, uint8_t *data);
+    void user_delay_ms(uint32_t period);
+    int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
+    int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len);
+
+    int8_t user_i2c_write8(uint8_t dev_id, uint8_t data);
+    int8_t user_i2c_read16(uint8_t dev_id, uint8_t *data);
+
 #ifdef __cplusplus
 }
-#endif
 #endif
