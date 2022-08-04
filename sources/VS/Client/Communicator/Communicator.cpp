57b1bdeb15737d7b86524ec9e575a14591a7d5f7
Sasha7b9Work <Phuntik-it@tut.by>
2022-08-04 13:32:53 +0300

    *

--- sources/VS/Client/Communicator/Communicator.cpp
+++ sources/VS/Client/Communicator/Communicator.cpp
@@ -1,12 +1,9 @@
 // 2022/04/29 13:56:27 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "Communicator/Communicator.h"
-#include "Communicator/USB/USB.h"
 #include "Communicator/HC12/HC12.h"
 
 
 void Communicator::Update()
 {
-    USB::Update();
-
     HC12::Update();
 }
--- sources/VS/Client/Communicator/USB/USB.cpp
+++
@@ -1,8 +0,0 @@
-// 2022/04/29 13:56:15 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
-#include "Communicator/USB/USB.h"
-
-
-void USB::Update()
-{
-
-}
