14412255a515a1631c8150f1db9ba7a4998a3179
Sasha7b9Work <Phuntik-it@tut.by>
2022-08-24 15:55:22 +0300

    '*

--- sources/VS/Client-PC/Utils/DynamicBuffer.h
+++ sources/VS/Client-PC/Utils/DynamicBuffer.h
@@ -7,10 +7,33 @@ template<int size_chunk>
 class DynamicBuffer
 {
 public:
+    DynamicBuffer();
+
     void Append(uint8 *data, int size);
-    void Size() const { return last - first; }
+
+    int Size() const
+    {
+        if (!buffer)
+        {
+            return 0;
+        }
+
+        return last - first;
+    }
+
     uint8 *Data() { return buffer + first; }
+
+    // Перевыделить память. При этом данные из буфера сохраняются
+    void Realloc(int size);
+
+    // Убрать num_bytes байт из начала буфера
+    void RemoveFirst(int num_bytes);
+
+    // Уплотнить буфер (убрать из начала пустые байты)
+    void Compact();
+
 private:
+
     uint8 *buffer;      // Здесь хранятся данные. 0, если память не выделена
     int    capacity;    // Столько байт выделено.
     int    first;       // Первый действительный элемент
