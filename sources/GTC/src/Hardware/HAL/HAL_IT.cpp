94c30b158805069a20b1f3ceb6f4959053ae3d90
Sasha7b9Work <Phuntik-it@tut.by>
2023-04-11 11:44:48 +0300

    refact

--- sources/GTC/src/Hardware/HAL/HAL_IT.cpp
+++ sources/GTC/src/Hardware/HAL/HAL_IT.cpp
@@ -1,7 +1,7 @@
 #include "defines.h"
 #include "Hardware/CDC/CDC.h"
 #include "Hardware/HAL/HAL.h"
-#include "Modules/HC12/HC12.h"
+#include "Hardware/Modules/HC12/HC12.h"
 #include <stm32f1xx_hal.h>
 
 
