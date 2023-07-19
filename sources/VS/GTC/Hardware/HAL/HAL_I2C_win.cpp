b44bfe66214338ffa578836208dbc5b8fbeaf655
Sasha7b9 <Sasha7b9@tut.by>
2023-07-19 21:17:38 +0300

    *

---
+++ sources/VS/GTC/Hardware/HAL/HAL_I2C_win.cpp
@@ -0,0 +1,15 @@
+// 2023/7/19 21:10:25 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Hardware/HAL/HAL.h"
+
+
+int8 HAL_I2C1::Read(uint8 dev_id, uint8 reg_addr, uint8 *reg_data, uint16 len)
+{
+    return 0;
+}
+
+
+int8 HAL_I2C1::Write(uint8 dev_id, uint8 reg_addr, uint8 *reg_data, uint16 len)
+{
+    return 0;
+}
