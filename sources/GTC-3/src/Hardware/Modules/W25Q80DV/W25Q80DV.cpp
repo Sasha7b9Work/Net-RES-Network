8704cd381a3cafc8af5ebf003a033205ace75d16
Sasha7b9Work <Phuntik-it@tut.by>
2023-08-07 09:57:49 +0300

    *

---
+++ sources/GTC-3/src/Hardware/Modules/W25Q80DV/W25Q80DV.cpp
@@ -0,0 +1,4 @@
+// 2023/08/07 09:57:09 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Hardware/Modules/W25Q80DV/W25Q80DV.h"
+
---
+++ sources/GTC-3/src/Hardware/Modules/W25Q80DV/W25Q80DV.h
@@ -0,0 +1,8 @@
+// 2023/08/07 09:56:54 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+namespace W25Q80DV
+{
+
+}
