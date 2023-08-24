d8b648ff4150c310dda6d0e341f1b26ab3e98f99
Sasha7b9Work <Phuntik-it@tut.by>
2023-08-24 09:41:58 +0300

    detrash

--- sources/TwoSensors/src/Display/Display.cpp
+++ sources/TwoSensors/src/Display/Display.cpp
@@ -64,10 +64,6 @@ namespace Display
     // Вывести одно измерение на весь экран
     static void DrawBigMeasure();
 
-    static void DrawTime();
-
-    static void DrawTest();
-
     namespace Buffer
     {
         static uint8 buffer[WIDTH * HEIGHT / 2];       // Четырёхбитный цвет
@@ -267,10 +263,6 @@ void Display::Update()
 
             DrawMeasures();
 
-            DrawTime();
-
-            DrawTest();
-
             if (need_redraw)
             {
                 EndScene();
@@ -330,42 +322,6 @@ void Display::DrawMeasures()
 
         ST7735::WriteBuffer(x - 1, y, width, height);
     }
-}
-
-
-void Display::DrawTime()
-{
-//    int width = 160;
-//    int height = 16;
-//    int y = 105;
-//
-//    Font::Set(TypeFont::_12_10);
-//    
-//    Rectangle(width, height).Fill(4, y - 1, Color::BLACK);
-//
-//    PackedTime time = HAL_RTC::GetTime();
-//
-//    String<>("%02d:%02d:%02d", time.hours, time.minutes, time.seconds).Draw(5, 105, Color::WHITE);
-//
-//    String<>("%02d:%02d:%04d", time.day, time.month, time.year).Draw(80, 105);
-//
-//    ST7735::WriteBuffer(0, y, width, height);
-}
-
-
-void Display::DrawTest()
-{
-    int width = 160;
-    int height = 16;
-    int y = 105;
-
-    Font::Set(TypeFont::_12_10);
-
-    Rectangle(width, height).Fill(4, y - 1, Color::BLACK);
-
-    bool result = W25Q80DV::Test::Result();
-
-    String<>(result ? "Память: OK" : "Память: FAIL").Draw(5, 105, result ? Color::GREEN : Color::RED);
 }
 
 
