482665b88029ab6677ae00a28bc19841dfe1bf8c
Sasha7b9Work <Phuntik-it@tut.by>
2023-09-08 11:48:20 +0300

    *

---
+++ sources/GTC-3/src/Storage/Storage.cpp
@@ -0,0 +1,5 @@
+// 2023/09/08 11:47:04 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Storage/Storage.h
+
+
---
+++ sources/GTC-3/src/Storage/Storage.h
@@ -0,0 +1,9 @@
+// 2023/09/08 11:46:53 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+namespace Storage
+{
+
+}
+
