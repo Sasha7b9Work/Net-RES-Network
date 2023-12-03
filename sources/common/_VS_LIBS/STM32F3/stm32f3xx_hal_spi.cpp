866cebb84f61ebc43e7b19744ba3a813c6aefd11
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2023-12-03 15:23:23 +0300

    *

--- sources/common/_VS_LIBS/STM32F3/stm32f3xx_hal_spi.cpp
+++ sources/common/_VS_LIBS/STM32F3/stm32f3xx_hal_spi.cpp
@@ -6,3 +6,15 @@ HAL_StatusTypeDef HAL_SPI_Init(SPI_HandleTypeDef * /*hspi*/)
 {
     return HAL_OK;
 }
+
+
+HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef * /*hspi*/, uint8_t * /*pData*/, uint16_t /*Size*/, uint32_t /*Timeout*/)
+{
+    return HAL_OK;
+}
+
+
+HAL_StatusTypeDef HAL_SPI_TransmitReceive(SPI_HandleTypeDef * /*hspi*/, uint8_t * /*pTxData*/, uint8_t * /*pRxData*/, uint16_t /*Size*/, uint32_t /*Timeout*/)
+{
+    return HAL_OK;
+}
